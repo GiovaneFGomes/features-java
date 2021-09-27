2bf361967f896deeaf34b553d27ebeeb1bc12709
Giovane <giovanefernandes35@gmail.com>
2021-09-26 22:15:59 -0300

    finishing examples of sealed classes

--- src/br/com/giovane/java17/sealed/GeometricFigure.java
+++ src/br/com/giovane/java17/sealed/GeometricFigure.java
@@ -1,34 +1,41 @@
 package br.com.giovane.java17.sealed;
 
-public sealed abstract class GeometricFigure permits Square, Circle, Triangle, Rectangle {
+// interfaces can be sealed
+sealed interface SealedInterface permits Circle, Square, Rectangle, Triangle { }
 
+// classes can be sealed
+public sealed abstract class GeometricFigure permits Circle, Rectangle, Square, Triangle {
     protected String color;
     public abstract void calculateArea();
-
 }
 
-final class Square extends GeometricFigure {
+// classes that extends and implements the sealed classes and interfaces
+non-sealed class Square extends GeometricFigure implements SealedInterface {
     @Override
     public void calculateArea() { }
 }
 
-
-final class Circle extends GeometricFigure {
+non-sealed class Circle extends GeometricFigure implements SealedInterface {
     @Override
     public void calculateArea() { }
 }
 
-
-final class Triangle extends GeometricFigure {
+final class Triangle extends GeometricFigure implements SealedInterface {
     @Override
     public void calculateArea() { }
 }
 
-
-final class Rectangle extends GeometricFigure {
+final class Rectangle extends GeometricFigure implements SealedInterface {
     @Override
     public void calculateArea() { }
 }
 
+//class Table extends GeometricFigure{ }
 
-// class Table extends GeometricFigure{}
+
+// 3 modifiers
+
+// final = no other class can extend.
+// sealed = The class is sealed and has to declare permits saying which classes are allowed.
+// non-sealed = is non-sealed, and other classes can extend.
+
